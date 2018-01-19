3dfaba74442450d5242604fc20658101af3dd061
Fin Le <finhoangle@gmail.com>
2018-01-19 15:27:24 -0800

    finish auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: '771786582202-1j8qgshc0f4530tcpicka06gji2d0o7s.apps.googleusercontent.com',
-    googleClientSecret: 'da-u46LaLMZoeQAIuaY3luQt',
-    mongoURI: 'mongodb://admin:admin@ds259117.mlab.com:59117/emaily-dev-kevin',
-    cookieKey: 'aylmao'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
