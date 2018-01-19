2369d5dd3f7bebeafc24a6f50cc83bd78dac1a06
Fin Le <finhoangle@gmail.com>
2018-01-19 14:57:04 -0800

    push to heroku

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '771786582202-1j8qgshc0f4530tcpicka06gji2d0o7s.apps.googleusercontent.com',
+    googleClientSecret: 'da-u46LaLMZoeQAIuaY3luQt',
+    mongoURI: 'mongodb://admin:admin@ds259117.mlab.com:59117/emaily-dev-kevin',
+    cookieKey: 'aylmao'
+};
